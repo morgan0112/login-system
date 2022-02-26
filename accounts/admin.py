1dbdd21c8490cf18eea3a44d31cfe2fa325766e7
morgan164 <morganvanderwesthuizen0112@gmail.com>
2022-02-26 16:48:30 +0200

    Update Account Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from accounts.models import User, LoginLog
+
+
+class UserAdminView(admin.ModelAdmin):
+    list_display = (
+        'username', 'first_name',
+        'last_name',
+        'email', 'is_superuser',
+        'is_active', 'date_joined',
+        'last_login'
+    )
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    list_filter = ('last_login', 'date_joined')
+
+
+class LoginLogAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date_login')
+    search_fields = ('user',)
+
 
 # Register your models here.
+admin.site.register(User, UserAdminView)
+admin.site.register(LoginLog, LoginLogAdmin)
